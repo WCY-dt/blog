6287091fe3d2cb57abf73252eee299aa4da3b444
WCY-dt <834421194@qq.com>
2024-06-04 20:19:03 +0800

    feat: Update style of post

--- assets/js/optimize.js
+++
@@ -1,8 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState === "interactive") {
-        var images = document.getElementsByTagName('img');
-        for (var i = 0; i < images.length; i++) {
-            images[i].setAttribute('loading', 'lazy');
-        }
-    }
-}
