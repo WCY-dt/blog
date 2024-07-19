c86024d45082c5c433ce42c25bcd5b10a2f059fa
WCY-dt <834421194@qq.com>
2024-07-19 14:16:24 +0800

    fix: Fix style problems and optimize

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,8 @@ const menuContainerMobile = document.querySelector('#menu-container-mobile');
 const menuContainerMobileButton = document.querySelector('#menu-container-mobile-button');
 const menuContainerMobileMenu = document.querySelector('#menu-container-mobile-menu');
 
+menuContainerMobileMenu.style.display = 'none';
+
 menuContainerMobile.addEventListener('click', () => {
     if (menuContainerMobileMenu.style.display === 'none') {
         menuContainerMobileMenu.style.display = 'flex';
---
+++ assets/js/optimize.js
@@ -0,0 +1,13 @@
+if (document.fonts) {
+    document.fonts.load('1em "Material Symbols Outlined"').then(() => {
+        document.querySelectorAll('.material-symbols-outlined').forEach(icon => {
+            icon.style.display = 'inline-block';
+        });
+    });
+} else {
+    window.onload = function () {
+        document.querySelectorAll('.material-symbols-outlined').forEach(icon => {
+            icon.style.display = 'inline-block';
+        });
+    }
+}
