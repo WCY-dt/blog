f8be7fca4c541bf57e29b329c78c2d12aaf3ab39
WCY-dt <834421194@qq.com>
2024-07-19 11:57:05 +0800

    feat: Add mobile menu

---
+++ assets/js/menu.js
@@ -0,0 +1,13 @@
+const menuContainerMobile = document.querySelector('#menu-container-mobile');
+const menuContainerMobileButton = document.querySelector('#menu-container-mobile-button');
+const menuContainerMobileMenu = document.querySelector('#menu-container-mobile-menu');
+
+menuContainerMobile.addEventListener('click', () => {
+    if (menuContainerMobileMenu.style.display === 'none') {
+        menuContainerMobileMenu.style.display = 'flex';
+        menuContainerMobileButton.innerHTML = 'close';
+    } else {
+        menuContainerMobileMenu.style.display = 'none';
+        menuContainerMobileButton.innerHTML = 'featured_play_list';
+    }
+});
