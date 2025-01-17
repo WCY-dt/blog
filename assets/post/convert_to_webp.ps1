e17e2eadcc085c1baf9d3d1fec43f2a25ad0a9d6
WCY-dt <834421194@qq.com>
2025-01-18 02:03:38 +0800

    doc: 深入Redis-part

--- assets/post/convert_to_webp.ps1
+++ assets/post/convert_to_webp.ps1
@@ -5,17 +5,19 @@ if (-Not (Test-Path -Path $outputFolder)) {
     New-Item -ItemType Directory -Path $outputFolder
 }
 
-$imageFiles = Get-ChildItem -Path $inputFolder -File -Include @("*.png", "*.gif", "*.jpeg", "*.jpg", "*.svg", "*.bmp", "*.tiff", "*.ico", "*.cur", "*.apng", "*.avif", "*.heif", "*.heic") -Recurse
-# foreach ($imageFile in $imageFiles) {
-#     Write-Output $imageFile.FullName
-# }
+$imageFiles = Get-ChildItem -Path $inputFolder -File -Include @("*.png", "*.gif", "*.jpeg", "*.jpg", "*.bmp", "*.tiff", "*.ico", "*.cur", "*.apng", "*.avif", "*.heif", "*.heic") -Recurse
+
+$convertSvg = Read-Host "Do you want to convert SVG files? (Y/N)"
+if ($convertSvg -eq "Y" -or $convertSvg -eq "y") {
+    $svgFiles = Get-ChildItem -Path $inputFolder -File -Include @("*.svg") -Recurse
+    $imageFiles += $svgFiles
+}
 
 if ($imageFiles.Count -eq 0) {
     Write-Output "No image files found in the input folder."
 } else {
     foreach ($imageFile in $imageFiles) {
         $outputFile = Join-Path -Path $outputFolder -ChildPath ($imageFile.BaseName + ".webp")
-        # Write-Output "Converting $($imageFile.FullName) to $outputFile"
         & cwebp $imageFile.FullName -o $outputFile
     }
     Write-Output "Conversion completed!"
