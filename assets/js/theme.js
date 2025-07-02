0e586e882ac525e46f397368904b116ce8b0af37
Ch3nyang <55525165+WCY-dt@users.noreply.github.com>
2025-07-03 00:16:23 +0800

    feat: Update theme settings and improve theme switching functionality

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,25 +1,15 @@
-const themeSwitcher = document.querySelector('#theme-switcher')
+---
+---
+const themeSwitcher = document.querySelector('#theme-switcher');
 
-function setTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme) // Use data-theme attribute on the html element
-    themeSwitcher.textContent = theme === 'light' ? 'dark_mode' : 'light_mode'
-    localStorage.setItem('theme', theme)
-    if (typeof setCommentTheme === 'function') {
-        setCommentTheme(theme)
-    }
-}
+function setTheme(theme) { document.documentElement.setAttribute('data-theme',
+theme); themeSwitcher.textContent = theme === 'light' ? 'dark_mode' :
+'light_mode'; localStorage.setItem('blog-theme', theme); if (typeof
+setCommentTheme === 'function') { setCommentTheme(theme); } }
 
-function applySystemTheme() {
-    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
-    setTheme(prefersDarkScheme.matches ? 'dark' : 'light')
-}
+const currentTheme = localStorage.getItem('blog-theme'); currentTheme ?
+setTheme(currentTheme) : setTheme('{{ site.color_theme | default: 'light' }}');
 
-const currentTheme = localStorage.getItem('theme')
-currentTheme ? setTheme(currentTheme) : applySystemTheme()
-
-themeSwitcher.addEventListener('click', () => {
-    const newTheme = document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' : 'light'
-    setTheme(newTheme)
-})
-
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', applySystemTheme)
+themeSwitcher.addEventListener('click', () => { const newTheme =
+document.documentElement.getAttribute('data-theme') === 'light' ? 'dark' :
+'light'; setTheme(newTheme) });
